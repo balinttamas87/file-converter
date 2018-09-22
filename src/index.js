94c84679f139f4537eed7e497677c6c8eec9a30a
balinttamas87 <balint.tamas87@gmail.com>
2018-09-22 23:05:23 +0100

    Setup Redux
    Add sample action and reducer
    
    log actions to the console

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const logger = createLogger({
+  collapsed: true,
+  diff: true
+});
+
+const START = "START";
+const start = () => ({ type: START });
+const initialState = { appStarted: false };
+
+const store = createStore((state = initialState, action) => {
+  switch (action.type) {
+    case START:
+      return { ...state, appStarted: true };
+    default:
+      return state;
+  }
+}, applyMiddleware(logger));
+
+store.dispatch(start());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
